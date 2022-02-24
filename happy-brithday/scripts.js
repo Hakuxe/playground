cbe4ac7954b850065a9ae0eb314332a5e266e06b
Hakuxe <arthurnorton.jobs@gmail.com>
2022-02-24 14:51:01 -0300

    criando modo de comemoração

--- happy-brithday/scripts.js
+++ happy-brithday/scripts.js
@@ -1,7 +1,8 @@
 // const eventDate = new Date("Feb 24, 2022 12:37:25");
 // const eventDate = new Date();
 
-
+const PREPARATION_IMAGE = "./assets/birthday_girl.svg";
+const CELEBRATION_IMAGE = "./assets/online_party.svg";
 
 const UTILS = {
 	formatTwoDigits(n) {
@@ -16,7 +17,61 @@ const UTILS = {
 			element.innerHTML = content;
 		}
 	},
+
+	updateElementAttributeById(elementId, content){
+		const element = document.getElementById(elementId);
+		if (!element) {
+			console.error(`Elemento com id: {${elementId}} não encontrado!!`);
+		} else {
+			element.setAttribute("src", content)  ;
+		}
+	},
 };
+
+const PAGESTYLE = {
+	button:document.querySelector(".primary-button"),
+	title:document.querySelector(".title"),
+	titleSpan:document.querySelector(".title span"),
+	celebrationTitle:document.querySelector(".celebration-title"),
+	message:document.querySelector(".message"),
+	countDown: document.querySelector(".countdown"),
+
+	reset(){
+		let { button, title,titleSpan, message, countDown, celebrationTitle} = PAGESTYLE;
+
+		title.classList.remove("mode-celebration");
+		button.classList.remove("mode-celebration");
+
+		titleSpan.classList.remove("invisible");
+		message.classList.remove("invisible");
+		countDown.classList.remove("invisible");
+		celebrationTitle.classList.add("invisible");
+		
+		UTILS.updateElementAttributeById("image-birthday", PREPARATION_IMAGE);
+	},
+
+	changeMode(){
+		let { button, title,titleSpan, message, countDown, celebrationTitle} = PAGESTYLE;
+
+		
+		//trocar as cores para => --pink
+		button.classList.toggle("mode-celebration");
+		title.classList.toggle("mode-celebration");
+		
+		//trocar os textos
+		titleSpan.classList.add("invisible");
+		message.classList.add("invisible");
+		countDown.classList.add("invisible");
+		
+
+		celebrationTitle.classList.remove("invisible");
+		
+		//trocar a img para => CELEBRATION_IMAGE
+		UTILS.updateElementAttributeById("image-birthday", CELEBRATION_IMAGE);
+
+	},
+
+}
 
 const MODAL = {
 	modal: document.querySelector(".modal-overlay"),
@@ -69,32 +124,42 @@ const CLOCKFUNCTIONS = {
 			UTILS.updateInnerHtmlById("minutes", dateParts.minutes);
 			UTILS.updateInnerHtmlById("seconds", dateParts.seconds);
 		}
+	
 
 		
 		if (timeRemaining < 0) {
 
-			//atualizar o html
-			UTILS.updateInnerHtmlById("days",UTILS.formatTwoDigits(dateParts.days));
-			UTILS.updateInnerHtmlById("hours",UTILS.formatTwoDigits(dateParts.hours));
-			UTILS.updateInnerHtmlById("minutes",UTILS.formatTwoDigits(dateParts.minutes));
-			UTILS.updateInnerHtmlById("seconds",UTILS.formatTwoDigits(dateParts.seconds));
+			if(	currentTime.getDate() == CLOCKFUNCTIONS.eventDate.getDate() &&
+			 	currentTime.getMonth() == CLOCKFUNCTIONS.eventDate.getMonth() && 
+			 	currentTime.getFullYear() == CLOCKFUNCTIONS.eventDate.getFullYear() 
+			){
+				console.log("é hj");
+				CLOCKFUNCTIONS.stopCountDown(CLOCKFUNCTIONS.idInterval);
+			
+				PAGESTYLE.changeMode();
+			}else{
+				// é ano que vem 
+
+				//FIXME:marcar da data para o próximo ano caso seja menor q a data atual 
+			}
 
 			//parar o setInverval
 			CLOCKFUNCTIONS.stopCountDown(CLOCKFUNCTIONS.idInterval);
-
+			
+			PAGESTYLE.changeMode();
 		}
 	},
-
+	
 	startCountDown() {
 		
 		// iniciar o timer
 		return setInterval(() => {
 			CLOCKFUNCTIONS.countDownTimer()
 		}, 1000);
-
+		
 		// return setInterval(CLOCKFUNCTIONS.countDownTimer(), 1000);
 	},
-
+	
 	stopCountDown(idInterval) {
 		clearInterval(idInterval)
 	},
@@ -121,10 +186,11 @@ const FORM = {
 		//ṕara qualquer intevalo existente (caso troca de data)
 		CLOCKFUNCTIONS.stopCountDown(CLOCKFUNCTIONS.idInterval);
 
-		//chamar o set inverval e iniciar o clock
+
+		PAGESTYLE.reset()
+
 		CLOCKFUNCTIONS.idInterval = CLOCKFUNCTIONS.startCountDown();
 		
-
 
 		//resetando o form após enviar
 		this.form.reset();
