c6a7e5087bf66ba4a745ba681c60b2266dd7a5c3
Hakuxe <arthurnorton.jobs@gmail.com>
2022-02-25 10:08:41 -0300

    finalizando projeto happy birthday

--- happy-brithday/scripts.js
+++ happy-brithday/scripts.js
@@ -1,6 +1,3 @@
-// const eventDate = new Date("Feb 24, 2022 12:37:25");
-// const eventDate = new Date();
-
 const PREPARATION_IMAGE = "./assets/birthday_girl.svg";
 const CELEBRATION_IMAGE = "./assets/online_party.svg";
 
@@ -18,26 +15,27 @@ const UTILS = {
 		}
 	},
 
-	updateElementAttributeById(elementId, content){
+	updateElementAttributeById(elementId, content) {
 		const element = document.getElementById(elementId);
 		if (!element) {
 			console.error(`Elemento com id: {${elementId}} não encontrado!!`);
 		} else {
-			element.setAttribute("src", content)  ;
+			element.setAttribute("src", content);
 		}
 	},
 };
 
 const PAGESTYLE = {
-	button:document.querySelector(".primary-button"),
-	title:document.querySelector(".title"),
-	titleSpan:document.querySelector(".title span"),
-	celebrationTitle:document.querySelector(".celebration-title"),
-	message:document.querySelector(".message"),
+	button: document.querySelector(".primary-button"),
+	title: document.querySelector(".title"),
+	titleSpan: document.querySelector(".title span"),
+	celebrationTitle: document.querySelector(".celebration-title"),
+	message: document.querySelector(".message"),
 	countDown: document.querySelector(".countdown"),
 
-	reset(){
-		let { button, title,titleSpan, message, countDown, celebrationTitle} = PAGESTYLE;
+	reset() {
+		let { button, title, titleSpan, message, countDown, celebrationTitle } =
+			PAGESTYLE;
 
 		title.classList.remove("mode-celebration");
 		button.classList.remove("mode-celebration");
@@ -46,32 +44,28 @@ const PAGESTYLE = {
 		message.classList.remove("invisible");
 		countDown.classList.remove("invisible");
 		celebrationTitle.classList.add("invisible");
-		
+
 		UTILS.updateElementAttributeById("image-birthday", PREPARATION_IMAGE);
 	},
 
-	changeMode(){
-		let { button, title,titleSpan, message, countDown, celebrationTitle} = PAGESTYLE;
+	changeModeForCelebration() {
+		let { button, title, titleSpan, message, countDown, celebrationTitle } = PAGESTYLE;
 
-		
 		//trocar as cores para => --pink
-		button.classList.toggle("mode-celebration");
-		title.classList.toggle("mode-celebration");
-		
+		button.classList.add("mode-celebration");
+		title.classList.add("mode-celebration");
+
 		//trocar os textos
 		titleSpan.classList.add("invisible");
 		message.classList.add("invisible");
 		countDown.classList.add("invisible");
-		
 
 		celebrationTitle.classList.remove("invisible");
-		
+
 		//trocar a img para => CELEBRATION_IMAGE
 		UTILS.updateElementAttributeById("image-birthday", CELEBRATION_IMAGE);
-
 	},
-
-}
+};
 
 const MODAL = {
 	modal: document.querySelector(".modal-overlay"),
@@ -83,6 +77,10 @@ const MODAL = {
 const CLOCKFUNCTIONS = {
 	eventDate: new Date(),
 	idInterval: 0,
+
+	calculateTimeRemaining(currentDate, eventDate){
+		return (eventDate - currentDate);
+	},
 
 	mountCountDownDate(valueInMillesecons) {
 		let parts = {
@@ -107,61 +105,57 @@ const CLOCKFUNCTIONS = {
 		CLOCKFUNCTIONS.eventDate = new Date(date.split("-"));
 	},
 
+	isEventToday(currentDate, eventDate){
+		return(
+			currentDate.getDate() == eventDate.getDate() &&
+			currentDate.getMonth() == eventDate.getMonth() &&
+			currentDate.getFullYear() == eventDate.getFullYear()
+		);
+	},
+
 	countDownTimer() {
-		const currentTime = new Date();
+		const currentDate = new Date();
 		let dateParts = { days: 0, hours: 0, minutes: 0, seconds: 0 };
 
-		let timeRemaining = Number(CLOCKFUNCTIONS.eventDate) - currentTime;
+		let timeRemaining = CLOCKFUNCTIONS.calculateTimeRemaining(currentDate, CLOCKFUNCTIONS.eventDate) ;
 
 		if (timeRemaining > 0) {
 			dateParts = CLOCKFUNCTIONS.mountCountDownDate(timeRemaining);
 
-			
-			//TODO : melhorar a atualização do html
-			
 			UTILS.updateInnerHtmlById("days", dateParts.days);
 			UTILS.updateInnerHtmlById("hours", dateParts.hours);
 			UTILS.updateInnerHtmlById("minutes", dateParts.minutes);
 			UTILS.updateInnerHtmlById("seconds", dateParts.seconds);
 		}
-	
 
-		
 		if (timeRemaining < 0) {
+			
+			if (CLOCKFUNCTIONS.isEventToday(currentDate, CLOCKFUNCTIONS.eventDate)) {
 
-			if(	currentTime.getDate() == CLOCKFUNCTIONS.eventDate.getDate() &&
-			 	currentTime.getMonth() == CLOCKFUNCTIONS.eventDate.getMonth() && 
-			 	currentTime.getFullYear() == CLOCKFUNCTIONS.eventDate.getFullYear() 
-			){
-				console.log("é hj");
 				CLOCKFUNCTIONS.stopCountDown(CLOCKFUNCTIONS.idInterval);
-			
-				PAGESTYLE.changeMode();
-			}else{
-				// é ano que vem 
 
-				//FIXME:marcar da data para o próximo ano caso seja menor q a data atual 
+				PAGESTYLE.changeModeForCelebration();
+
+			} else {
+				// caso a data já tenha passado, marca como ano que vem
+				CLOCKFUNCTIONS.eventDate = new Date(
+					CLOCKFUNCTIONS.eventDate.getFullYear() + 1,
+					CLOCKFUNCTIONS.eventDate.getMonth(),
+					CLOCKFUNCTIONS.eventDate.getDate()
+				);
 			}
-
-			//parar o setInverval
-			CLOCKFUNCTIONS.stopCountDown(CLOCKFUNCTIONS.idInterval);
-			
-			PAGESTYLE.changeMode();
 		}
 	},
-	
+
 	startCountDown() {
-		
 		// iniciar o timer
 		return setInterval(() => {
-			CLOCKFUNCTIONS.countDownTimer()
+			CLOCKFUNCTIONS.countDownTimer();
 		}, 1000);
-		
-		// return setInterval(CLOCKFUNCTIONS.countDownTimer(), 1000);
 	},
-	
+
 	stopCountDown(idInterval) {
-		clearInterval(idInterval)
+		clearInterval(idInterval);
 	},
 };
 
@@ -180,22 +174,16 @@ const FORM = {
 		//pegando os valores enviados no form
 		let { name, birthday } = this.getValues();
 
-		
 		CLOCKFUNCTIONS.setEventDate(birthday);
 
-		//ṕara qualquer intevalo existente (caso troca de data)
+		// eleminar qualquer intevalo existente (caso troca de data)
 		CLOCKFUNCTIONS.stopCountDown(CLOCKFUNCTIONS.idInterval);
-
-
-		PAGESTYLE.reset()
+		PAGESTYLE.reset();
 
 		CLOCKFUNCTIONS.idInterval = CLOCKFUNCTIONS.startCountDown();
-		
 
 		//resetando o form após enviar
 		this.form.reset();
 		MODAL.toggle();
 	},
 };
-
-
