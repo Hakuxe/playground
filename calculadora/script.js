56cb407ab3db2ee2e351f31bd92ed5803654079f
Hakuxe <arthurnorton.jobs@gmail.com>
2022-04-11 16:03:12 -0300

    criando funcoes matematicas

--- calculadora/script.js
+++ calculadora/script.js
@@ -1,13 +1,41 @@
 /**
  * Element selector: querySelector
- * @param {string} element - element identifier 
+ * @param {string} element - element identifier
+ * @param {boolean} [all] - select all elements with identifier
  * @return {string} returns the first node that matches the identifier
  *
  * @example
  *     $('input')
  */
 
-function $(element){
-   return( document.querySelector(element));
+function $(element, all) {
+	if (all) {
+		return document.querySelectorAll(element);
+	} else {
+		return document.querySelector(element);
+	}
 }
 
+
+
+
+
+
+const sum = (number1, number2) => {
+	return Number(number1) + Number(number2);
+};
+
+const subtract = (number1, number2) => {
+	return Number(number1) - Number(number2);
+};
+
+const multiply = (number1, number2) => {
+	return Number(number1) - Number(number2);
+};
+
+const divide = (number1, number2) => {
+   if(number2 == 0){
+      return null
+   }
+	return Number(number1) / Number(number2);
+};
