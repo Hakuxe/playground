ce431d1347c747720f2a71fe1eb8b87242aa24fc
Hakuxe <arthurnorton.jobs@gmail.com>
2022-05-17 19:23:37 -0300

    criando busca de user por id

--- api-users/server/server.js
+++ api-users/server/server.js
@@ -7,23 +7,40 @@
 
 */
 
-import express from 'express';
-import cors from 'cors'
-const PORT = 4000
+import express from "express";
+import cors from "cors";
+const PORT = 4000;
 const app = express();
 
 app.use(express.json());
 app.use(cors());
 
-app.listen(PORT, console.log('rodando....'));
+app.listen(PORT, console.log("rodando...."));
 
-let users = [{
-   id: 1,
-   name: "Caio Augusto",
-   city: "São Paulo"
- }]
- 
- 
- app.get('/users',(req, res) => res.json({
-   users
- }))
+let users = [
+	{
+		id: 1,
+		name: "Caio Augusto",
+		city: "São Paulo",
+	},
+	{
+		id: 2,
+		name: "Henrrique Augusto",
+		city: "Belo Horizonte",
+	},
+];
+
+app.get("/users", (req, res) =>
+	res.json({
+		users,
+	})
+);
+
+app.get("/users/:id", (req, res) => {
+	const { id } = req.params;
+
+  const userSelected = users.filter((item) => Number(item.id) === Number(id));
+
+  res.send(userSelected);
+
+});
